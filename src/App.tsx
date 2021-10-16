f83cee488b8f5f1ccd60c10e6dece77d85693271
sowlomon <solomonadache@gmail.com>
2021-10-16 17:34:47 +0100

    added stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
+const name:string = "SOLOMON "
+
+const addNum =  (name : string): string|number => {
+  if(name === "pedro"){
+    return 20
+  }else{
+    return "Name not equal to pedro"
+  }
+}
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <h1>{addNum("Godwin")}</h1>
     </div>
   );
 }
